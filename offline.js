07525d1f193bd7f05b1b1aba27f48e55a05cf0c1
nonglakkoonmee <45388066+nonglakkoonmee@users.noreply.github.com>
2025-11-25 22:21:55 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1764083053,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player2-sheet0.png",
+		"images/obj_floor-sheet0.png",
+		"images/obj_bg-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/golditem-sheet0.png",
+		"images/obj_turret-sheet0.png",
+		"images/obj_bullet-sheet0.png",
+		"images/obj_bg1-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
